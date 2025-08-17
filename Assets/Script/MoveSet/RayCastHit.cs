32414cefa075c534dc775a7b99105debd9dd8462
Thy-E-d-na <thyhoang313@gmail.com>
2025-08-17 12:09:54 +0700

    update

--- Assets/Script/MoveSet/RayCastHit.cs
+++ Assets/Script/MoveSet/RayCastHit.cs
@@ -7,12 +7,18 @@ public class RayCastHit : MonoBehaviour
     [SerializeField] private Transform _chargePos;
     //noi cast phep tao skill
     [SerializeField] private GameObject _effectPref;
+    [SerializeField] private int _damage;
     public void Shocking()
     {
         Ray ray = new(_aimingPos.position, _chargePos.position - _aimingPos.position);
         if (Physics.Raycast(ray, out var rayCasthit))
         {
             Instantiate(_effectPref, rayCasthit.point, Quaternion.LookRotation(rayCasthit.normal));
+            if (rayCasthit.collider.TryGetComponent<Health>(out var health))
+            {
+                health.TakeDamage(_damage);
+            }
         }
+        
     }
 }
---
+++ Assets/Script/takeDmg.cs
@@ -0,0 +1,11 @@
+using UnityEditor.ShaderGraph.Internal;
+using UnityEngine;
+
+public class takeDmg : MonoBehaviour
+{
+    [SerializeField] private float _dmg;
+    public void dmg()
+    {
+
+    }
+}
